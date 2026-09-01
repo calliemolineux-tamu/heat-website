84e7a345f9b912efa2d2fa1fdc43e53a18a307b3
Callie Molineux <callie.molineux@laminarinnovation.com>
2026-09-01 13:56:25 -0500

    fixed some issues with previous cached calendar

--- public/assets/controllers/photo_controller-a9f2d46517db0ca00fec848caef72881820107ab2fbc4f1ee67c07842026e08a.js
+++
@@ -1,8 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-
-export default class extends Controller {
-  closeForm(event) {
-    event.preventDefault();
-    this.element.closest("turbo-frame").remove();
-  }
-};
